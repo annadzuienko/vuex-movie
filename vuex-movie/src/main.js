8633267d14c09baa759817a45cbc51a9c60edd3e
Anna Dzuienko <annadzuienko@MacBook-Anna.local>
2019-05-16 16:13:55 +0300

    Install router & axios

--- vuex-movie/src/main.js
+++ vuex-movie/src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex';
 import 'es6-promise/auto';
+import VueRouter from 'vue-router';
+import axios from "axios";
 
 Vue.use(Vuex);
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
